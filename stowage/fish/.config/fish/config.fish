1997969ec096c11c6b6f957617868c76b6232209
Jamie van Dyke <jamie@fearof.fish>
2022-01-24 11:46:32 +0000

    fishy fishy fishy

--- stowage/fish/.config/fish/config.fish
+++ stowage/fish/.config/fish/config.fish
@@ -2,16 +2,19 @@ if status is-interactive
   fish_add_path -am /usr/bin /sbin /usr/sbin /bin
   fish_add_path -pm /usr/local/bin /usr/local/sbin
   fish_add_path -pm /opt/homebrew/sbin /opt/homebrew/bin 
-  fish_add_path -pm ~/.local/bin ~/bin 
+  fish_add_path -pm ~/.local/bin ~/bin
   source /opt/homebrew/opt/asdf/libexec/asdf.fish
+  fish_add_path -pm ~/.asdf/shims
   fish_add_path -pm ~/go/bin
 end
 
 set -Ux PAGER less
 set -Ux EDITOR 'lvim'
 set -Ux VISUAL 'lvim'
+set -Ux GPG_TTY (tty)
 
 alias l="lvim"
+alias n="nova"
 
 starship init fish | source
 
