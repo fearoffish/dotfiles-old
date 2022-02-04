ed90c4724281e6a6cb2484f7bda26802812f26fb
Jamie van Dyke <jamie@fearof.fish>
2022-02-04 13:40:39 +0000

    update fish config

--- stowage/fish/.config/fish/config.fish
+++ stowage/fish/.config/fish/config.fish
@@ -1,7 +1,10 @@
+function fish_greeting
+end
+
 if status is-interactive
   fish_add_path -am /usr/bin /sbin /usr/sbin /bin
   fish_add_path -pm /usr/local/bin /usr/local/sbin
-  fish_add_path -pm /opt/homebrew/sbin /opt/homebrew/bin 
+  fish_add_path -pm /opt/homebrew/sbin /opt/homebrew/bin
   fish_add_path -pm ~/.local/bin ~/bin
   source /opt/homebrew/opt/asdf/libexec/asdf.fish
   fish_add_path -pm ~/.asdf/shims
@@ -9,12 +12,13 @@ if status is-interactive
 end
 
 set -Ux PAGER less
-set -Ux EDITOR 'lvim'
-set -Ux VISUAL 'lvim'
+set -Ux EDITOR 'subl -w'
+set -Ux VISUAL 'subl -w'
 set -Ux GPG_TTY (tty)
 
 alias l="lvim"
 alias n="nova"
+alias s="subl"
 
 starship init fish | source
 
